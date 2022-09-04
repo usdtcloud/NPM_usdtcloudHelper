8f73e71f84792f9a883b0da25a0b1d6b30d1a31f
usdtcloud <usdtcloud@gmail.com>
2022-09-04 15:09:03 +0800

    修复

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Math = require('./src/Math/index')
 const Time = require('./src/Time/index')
+const String = require('./src/String/index')
 
 module.exports = {
     Math:Math,
     Time:Time,
+    String:String,
 }
